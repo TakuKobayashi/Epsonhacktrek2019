935d8cb26a42eba9f3a997f1279cb409f1c4bd19
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-16 23:02:50 +0900

    勢いでflaskからinputでデータの入力ができるページを作成した

--- wordcloud/server.py
+++ wordcloud/server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+# coding:utf-8
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    hello = "Hello world"
-    return hello
+@app.route("/", methods=["GET"])
+def index():
+  return render_template('index.html', title='sample page')
+
+@app.route("/generate", methods=["POST"])
+def generate():
+  sentence = request.form['sentence']
+  return render_template('index.html', title=sentence)
 
 if __name__ == "__main__":
-    app.run()
+  app.run(debug=True, host='0.0.0.0', threaded=True)
