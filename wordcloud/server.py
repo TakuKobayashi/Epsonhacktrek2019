fe8dc614dfbbeacab28483dcc6536b48332b3660
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-16 23:28:28 +0900

    flask serverで画像を生成できるようにした

--- wordcloud/server.py
+++ wordcloud/server.py
@@ -1,6 +1,9 @@
 # coding:utf-8
 
 from flask import Flask, render_template, request
+from wordcloud import WordCloud
+import uuid
+import MeCab
 
 app = Flask(__name__)
 
@@ -11,7 +14,20 @@ def index():
 @app.route("/generate", methods=["POST"])
 def generate():
   sentence = request.form['sentence']
-  return render_template('index.html', title=sentence)
+  mecab = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+  nodes = mecab.parseToNode(sentence)
+  s = []
+  while nodes:
+    if nodes.feature[:2] == '名詞':
+      s.append(nodes.surface)
+    nodes = nodes.next
+  wc = WordCloud(width=480, height=320, background_color="white",
+                 stopwords={"もの","これ","ため","それ","ところ","よう"},
+                 font_path="/System/Library/Fonts/ヒラギノ角ゴシック W6.ttc")
+  wc.generate(" ".join(s))
+  image_file_name = str(uuid.uuid4()) + ".png"
+  wc.to_file(image_file_name)
+  return render_template('index.html', title=sentence, image=image_file_name)
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', threaded=True)
